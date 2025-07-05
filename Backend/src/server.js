0c55ab83ab4b73e1dc60f0d92b6d5748a2d1fd66
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-05 22:57:17 +0200

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,8 +1,28 @@
 import dotenv from 'dotenv';
-import app    from './app.js';
+import http from 'http';
+import app from './app.js';
+import { Server as IOServer } from 'socket.io';
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = http.createServer(app);
+
+// Configura Socket.IO
+export const io = new IOServer(server, {
+  cors: { origin: '*' }
+});
+
+io.on('connection', socket => {
+  console.log('👤 Cliente Socket conectado:', socket.id);
+  socket.on('joinVehicle', vehicleId => {
+    socket.join(`vehicle_${vehicleId}`);
+  });
+  socket.on('leaveVehicle', vehicleId => {
+    socket.leave(`vehicle_${vehicleId}`);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`🚀 Servidor backend a correr na porta ${PORT}`);
 });

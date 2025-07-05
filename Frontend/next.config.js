16a2e19d5c9614b2367803a35f4ed005da5cfc80
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-05 13:04:23 +0200

    Update next.config.js

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  // Garante que cada rota gera um HTML estático standalone
+  exportTrailingSlash: true,
   env: {
     NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API || 'http://localhost:5000'
   }

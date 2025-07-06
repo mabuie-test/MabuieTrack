6b93799087dc3ba66bcdc565c27e848658fdd548
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-06 19:16:45 +0200

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import { connectDB }          from './config/db.js';
-import authRoutes            from './routes/auth.js';
-import userRoutes            from './routes/users.js';
-import vehicleRoutes         from './routes/vehicles.js';
+import cors    from 'cors';
+import morgan  from 'morgan';
+import { connectDB } from './config/db.js';
+import authRoutes   from './routes/auth.js';
+import userRoutes   from './routes/users.js';
+import vehicleRoutes from './routes/vehicles.js';
 
 const app = express();
 connectDB();
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use('/api/auth', authRoutes);
+app.use('/api/auth',  authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/vehicles', vehicleRoutes);
 

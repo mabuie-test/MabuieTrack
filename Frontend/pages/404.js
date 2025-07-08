3226a3916e4af0ec0c5b19cf709161f00d9c4c41
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-08 20:25:02 +0200

    Create 404.js

---
+++ Frontend/pages/404.js
@@ -0,0 +1,14 @@
+// pages/404.js
+export default function Custom404() {
+  return (
+    <div style={{ padding:'2rem', textAlign:'center' }}>
+      <h1>404 — Página não encontrada</h1>
+      <p>Desculpe, a página que procuras não existe.</p>
+    </div>
+  );
+}
+
+// Força SSR, impedindo o export estático
+export async function getServerSideProps() {
+  return { props: {} };
+}

8ba35b98c35b7956c8ee2087fcf6f31f91f9a3bc
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-08 13:39:02 +0200

    Update login.js

--- Frontend/pages/login.js
+++ Frontend/pages/login.js
@@ -1,10 +1,11 @@
+// pages/login.js
 import { useState, useContext } from 'react';
 import { AuthContext } from '../src/contexts/AuthContext';
 
 export default function LoginPage() {
   const { login } = useContext(AuthContext);
   const [email, setEmail] = useState('');
-  const [pw, setPw]     = useState('');
+  const [pw, setPw]       = useState('');
 
   const handleSubmit = async e => {
     e.preventDefault();

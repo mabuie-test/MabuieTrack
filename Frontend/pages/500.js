b2dc68c2743576da934ae3468608214c9199ca7c
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-08 20:25:39 +0200

    Create 500.js

---
+++ Frontend/pages/500.js
@@ -0,0 +1,14 @@
+// pages/500.js
+export default function Custom500() {
+  return (
+    <div style={{ padding:'2rem', textAlign:'center' }}>
+      <h1>500 — Erro interno</h1>
+      <p>Algo correu mal no servidor. Tenta novamente mais tarde.</p>
+    </div>
+  );
+}
+
+// Força SSR
+export async function getServerSideProps() {
+  return { props: {} };
+}

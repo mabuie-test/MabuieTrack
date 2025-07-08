c8b0cc5f5bd09ccee8652f2a31d909e259c01008
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-08 20:50:54 +0200

    Create index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { AuthProvider } from './contexts/AuthContext';
+import './index.css'; // se quiseres algum CSS global
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>
+);

312a1ec020de4e7243e8fd2be839edf030fd5fab
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-08 22:51:16 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+// altera aqui
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 //import './index.css'; // se quiseres algum CSS global
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <BrowserRouter>
+    {/* usa HashRouter em vez de BrowserRouter */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </AuthProvider>
 );

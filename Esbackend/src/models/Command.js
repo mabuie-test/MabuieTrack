c169dcf58f684ddc816928a1f639fcd66e6b188e
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-09 16:10:10 +0200

    Create Command.js

---
+++ Esbackend/src/models/Command.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const CommandSchema = new mongoose.Schema({
+  cmd: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Command', CommandSchema);

84ebe385dc2b051bf4a356ef662afc882deba46a
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-09 13:25:41 +0200

    Create tailwind.config.js

---
+++ vehicle-tracking-frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: { extend: {} },
+  plugins: []
+};

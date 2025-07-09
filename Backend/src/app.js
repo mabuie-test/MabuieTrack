67e6f6e276fac6e719fd70a2a90967710adc497f
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-09 14:38:34 +0200

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors    from 'cors';
 import morgan  from 'morgan';
-import { connectDB } from './config/db.js';
-import authRoutes   from './routes/auth.js';
-import userRoutes   from './routes/users.js';
+import { connectDB }  from './config/db.js';
+import authRoutes    from './routes/auth.js';
+import userRoutes    from './routes/users.js';
 import vehicleRoutes from './routes/vehicles.js';
 
 const app = express();
@@ -13,8 +13,8 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use('/api/auth',  authRoutes);
-app.use('/api/users', userRoutes);
+app.use('/api/auth',    authRoutes);
+app.use('/api/users',   userRoutes);
 app.use('/api/vehicles', vehicleRoutes);
 
 app.get('/', (_, res) => res.json({ message: 'Backend a funcionar' }));

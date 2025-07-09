39dddf848eebe2b523ca13fc61dfa0657f2d9efb
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-09 12:42:53 +0200

    Delete vehicle-tracking-static directory

--- vehicle-tracking-static/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: (process.env.REACT_APP_API || 'http://localhost:5000') + '/api',
-  headers: { 'Content-Type': 'application/json' }
-});
-
-export default api;

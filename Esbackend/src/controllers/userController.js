7fab71ce651ac0f29a29df60aff8904dc839988f
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-09 16:06:07 +0200

    Create userController.js

---
+++ Esbackend/src/controllers/userController.js
@@ -0,0 +1,22 @@
+const User = require('../models/User');
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find().select('-password');
+    res.json(users);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Erro no servidor');
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select('-password');
+    if (!user) return res.status(404).json({ msg: 'Usuário não encontrado' });
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Erro no servidor');
+  }
+};
